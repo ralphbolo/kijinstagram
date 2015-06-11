8a8c4654813bbf260e480adfbde1b8d9ac06c863
ralphbolo <rbolo062@uottawa.ca>
2015-06-11 00:34:17 -0400

    item form is now working

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,18 @@ class ItemsController < ApplicationController
 	end
 
 	def create
-		
+		@item = Item.new(item_params)
+		if @item.save
+			#to do redirect to items index
+			redirect_to root_path
+		else
+			render 'new'
+		end
 	end
+
+	private
+
+		def item_params
+			params.require(:item).permit(:title, :price, :description, :address, :postal_code)
+		end
 end
